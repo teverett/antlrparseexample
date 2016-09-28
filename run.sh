35e9ecb80a8f81be437e51f5940655f49396f04e
Tom Everett <tom@khubla.com>
2016-09-27 19:00:23 -0600

    fixes

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -jar target/antlrparseexample-1.0.jar
--- src/main/java/com/khubla/antlrparseexample/main/Main.java
+++ src/main/java/com/khubla/antlrparseexample/main/Main.java
@@ -2,11 +2,6 @@ package com.khubla.antlrparseexample.main;
 
 import java.io.InputStream;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
 import com.khubla.antlrparseexample.DocumentParser;
 
 /**
@@ -17,18 +12,7 @@ public class Main {
     * main
     */
    static public void main(String args[]) throws Exception {
-      final String url = args[0];
-      System.out.println("Fetching " + url);
-      final CloseableHttpClient httpclient = HttpClients.createDefault();
-      final HttpGet httpGet = new HttpGet(url);
-      final CloseableHttpResponse response = httpclient.execute(httpGet);
-      InputStream inputStream = null;
-      try {
-         inputStream = response.getEntity().getContent();
-         System.out.println("Parsing " + url);
-         DocumentParser.parse(inputStream);
-      } finally {
-         response.close();
-      }
+      InputStream inputStream = Main.class.getResourceAsStream("/Java8Parser.java");
+      DocumentParser.parse(inputStream);
    }
 }
